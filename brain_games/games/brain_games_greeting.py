d608e040022a9723928288106a4fe8591b332118
Sergey Kalye <reyangood@gmail.com>
2023-01-14 16:03:15 +0300

    add brain_games_greeting.py

---
+++ brain_games/games/brain_games_greeting.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from brain_games.cli import welcome_user
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
