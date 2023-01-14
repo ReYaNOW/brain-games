9ae7a220422c13689eb183667e5883b34d4a5e8f
Sergey Kalye <reyangood@gmail.com>
2023-01-14 16:01:04 +0300

    add brain_calc.py

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import random
+import brain_games.cli
+from brain_games.scripts.brain import solver, wrong_answer
+
+
+def correct(a, b, symb):
+    if symb == '+':
+        return str(a + b)
+    if symb == '-':
+        return str(a - b)
+    if symb == '*':
+        return str(a * b)
+
+
+def main():
+    brain_games.cli.welcome_user()
+    print('What is the result of the expression?')
+    SYMBOLS = ['+', '-', '*']
+    counter = 0
+    THREE_TIMES = 3
+    while counter < THREE_TIMES:
+        counter += 1
+        rn_sy = random.choice(SYMBOLS)
+        n1 = random.randint(1, 100)
+        n2 = random.randint(1, 15)
+        if solver(f'{n1} {rn_sy} {n2}', correct(n1, n2, rn_sy)) == 'Correct!':
+            print('Correct!')
+        else:
+            return wrong_answer()
+
+    print(f'Congratulations, {brain_games.cli.name}!')
+
+
+if __name__ == '__main__':
+    main()
