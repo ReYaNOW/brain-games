40d4dedcf92e1937ad1c65999b38fb15669b2309
Sergey Kalye <reyangood@gmail.com>
2023-02-07 12:40:31 +0300

    change brain.py, cuz >79 column

--- brain_games/scripts/brain.py
+++ brain_games/scripts/brain.py
@@ -3,13 +3,13 @@ import brain_games.cli
 
 
 def solver(question, correct_answer):
-    global user_answer, correct
+    global user_answ, correct
     correct = correct_answer
-    user_answer = prompt.string(f'Question: {question} \nYour answer: ')
-    if user_answer == correct:
+    user_answ = prompt.string(f'Question: {question} \nYour answer: ')
+    if user_answ == correct:
         return 'Correct!'
 
 
 def wrong_answer():
-    print(f'"{user_answer}" is wrong answer ;(. Correct answer was "{correct}"')
+    print(f'"{user_answ}" is wrong answer ;(. Correct answer was "{correct}"')
     return print(f"Let's try again, {brain_games.cli.name}!")
