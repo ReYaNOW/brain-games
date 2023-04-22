b917135666914727a92e502850255b5c752b4c71
Sergei <reyangood@gmail.com>
2023-04-23 01:27:23 +0300

    fix linter errors in brain_calc.py

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -6,18 +6,18 @@ from brain_games.scripts.brain import solver
 
 def calc(a: int, b: int, operator: str):
     match operator:
-        case '+':
-            return str(a+b)
-        case '-':
-            return str(a-b)
-        case '*':
-            return str(a*b)
+        case "+":
+            return str(a + b)
+        case "-":
+            return str(a - b)
+        case "*":
+            return str(a * b)
 
 
 def main():
     brain_games.cli.welcome_user()
-    print('What is the result of the expression?')
-    SYMBOLS = ['+', '-', '*']
+    print("What is the result of the expression?")
+    SYMBOLS = ["+", "-", "*"]
     counter = 0
     THREE_TIMES = 3
     while counter < THREE_TIMES:
@@ -25,13 +25,13 @@ def main():
         rn_sy = random.choice(SYMBOLS)
         n1 = random.randint(1, 100)
         n2 = random.randint(1, 15)
-        if solver(f'{n1} {rn_sy} {n2}', calc(n1, n2, rn_sy)) == 'Correct!':
-            print('Correct!')
+        if solver(f"{n1} {rn_sy} {n2}", calc(n1, n2, rn_sy)) == "Correct!":
+            print("Correct!")
         else:
             return None
 
-    print(f'Congratulations, {brain_games.cli.name}!')
+    print(f"Congratulations, {brain_games.cli.name}!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
