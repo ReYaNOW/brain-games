9f5c819283e42499e1c0184afcdd64b498410c8b
Sergei <reyangood@gmail.com>
2023-09-02 01:41:03 +0300

    add module with consts called consts.py

---
+++ brain_games/consts.py
@@ -0,0 +1,7 @@
+three_times = 3
+
+even_desc = 'Answer "yes" if the number is even, otherwise answer "no".'
+calc_desc = "What is the result of the expression?"
+gcd_desc = "Find the greatest common divisor of given numbers."
+prime_desc = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+progression_desc = "What number is missing in the progression?"
