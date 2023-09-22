cb2e15d96c99ccdaa45a9581dc2f566ebda2a84f
Sergei <reyangood@gmail.com>
2023-09-22 23:30:25 +0300

    code refactoring in even.py and fix typo in setup.cfg

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -10,7 +10,7 @@ def is_even(number):
 def get_even_expression_and_answer() -> tuple[int, str]:
     number = get_random_int()
     answer = 'yes' if is_even(number) else 'no'
-    return number, str(answer)
+    return number, answer
 
 
 def start_even_game():
